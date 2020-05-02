747f3a68c051fe96aa733bfdd0100b9d4e440d13
TejaSreenivas <teja.sreenivas.05@gmail.com>
2020-05-02 14:23:30 -0500

    AllActivities bug fixes
    Displaying all activities bugs fixed.
    clear variable naming, bug fixes, ready to be merged into master branch

--- adventure-club/src/Components/AllActivities.js
+++ adventure-club/src/Components/AllActivities.js
@@ -13,121 +13,129 @@ function AllActivities() {
     1:"Yes",
     0:"No"
   }
+  const [state, setState] = React.useState({
+    columns: columns,
+    data: info
+  });
 
   const delete_row = (id)=>{
     // console.log(id.split('-'));
     var param = id.split('-');
-    var i;
-    for(i=0;i<org_data.length;i++){
-      if(org_data[i]._id == param[0]){
-        org_data[i].flag[parseInt(param[1])] = 0;
-        var updated_club = Object.assign({}, org_data[i]);
-        delete updated_club['_id'];
-        console.log(org_data[i]);
-        fetch('http://localhost:3000/clubs/'+param[0], {
-          method: "put",
-          headers: {
-              "Content-Type": "application/json; charset=utf-8"
-          },
-          body: JSON.stringify(updated_club)
-        })
-        break;
-      }
-    }
+    var club_id = param[0], activity_index = param[1];
+    fetch('http://localhost:3000/clubs/'+club_id, { method: "get" }).then((response) => {
+      return response.json();
+     }).then((club)=>{
+      club.activities[activity_index].status = 0;
+      delete club['_id'];
+      return club;
+     }).then((club)=>{
+      console.log(club);
+      return fetch('http://localhost:3000/clubs/'+club_id, {
+                  method: "put",
+                  headers: {
+                      "Content-Type": "application/json; charset=utf-8"
+                  },
+                  body: JSON.stringify(club)
+              });
+     });
     
   }
 
   const edit_row = (data)=>{
     // console.log(id.split('-'));
     var param = data.id.split('-');
-    var i;
-    for(i=0;i<org_data.length;i++){
-      if(org_data[i]._id == param[0]){
-        org_data[i].aname[parseInt(param[1])] = data.name;
-        org_data[i].adesc[parseInt(param[1])] = data.desc;
-        org_data[i].acat[parseInt(param[1])] = data.cat;
-        var updated_club = Object.assign({}, org_data[i]);
-        delete updated_club['_id'];
-        console.log(org_data[i]);
-        fetch('http://localhost:3000/clubs/'+param[0], {
-          method: "put",
-          headers: {
-              "Content-Type": "application/json; charset=utf-8"
-          },
-          body: JSON.stringify(updated_club)
-        })
-        break;
-      }
-    }
-    
+    var club_id = param[0], activity_index = param[1];
+    fetch('http://localhost:3000/clubs/'+club_id, { method: "get" }).then((response) => {
+      return response.json();
+     }).then((club)=>{
+      club.activities[activity_index].status = 1;
+      delete club['_id'];
+      club.activities[activity_index].name = data.name;
+      club.activities[activity_index].description = data.description;
+      club.activities[activity_index].category = data.category;
+      club.activities[activity_index].booking_needed = data.booking_needed;
+      return club;
+     }).then((club)=>{
+      // console.log(club);
+      return fetch('http://localhost:3000/clubs/'+club_id, {
+                method: "put",
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                },
+                body: JSON.stringify(club)
+              })
+     });
   }
 
-  const new_row = (new_amenity) =>{
-    var param = new_amenity.cname;
-    var i;
-    for(i=0;i<org_data.length;i++){
-      if(org_data[i]._id == param){
-        org_data[i].aname.push(new_amenity.name);
-        org_data[i].adesc.push(new_amenity.desc);
-        org_data[i].acat.push(new_amenity.cat);
-        org_data[i].flag.push(1);
-        var updated_club = Object.assign({}, org_data[i]);
-        delete updated_club['_id'];
-        console.log(updated_club);
-        fetch('http://localhost:3000/clubs/'+param, {
-          method: "put",
-          headers: {
-              "Content-Type": "application/json; charset=utf-8"
-          },
-          body: JSON.stringify(updated_club)
-        })
-        console.log(param+"-"+((updated_club.aname.length-1).toString()));
-        return param+"-"+((updated_club.aname.length-1).toString());
-      }
-    }
-    return 0;
+  const new_row = (data)=>{
+    var club_id = data.club_name;
+    fetch('http://localhost:3000/clubs/'+club_id, { method: "get" }).then((response) => {
+      return response.json();
+     }).then((club)=>{
+      delete club['_id'];
+      var activity_index = club.activities.length;
+      club.activities.push({});
+      club.activities[activity_index].status = 1;
+      club.activities[activity_index].name = data.name;
+      club.activities[activity_index].description = data.description;
+      club.activities[activity_index].category = data.category;
+      club.activities[activity_index].booking_needed = data.booking_needed;
+      return club;
+     }).then((club)=>{
+      // console.log(club);
+      return fetch('http://localhost:3000/clubs/'+club_id, {
+                method: "put",
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                },
+                body: JSON.stringify(club)
+              })
+     });
   }
 
-  fetch('http://localhost:3000/clubs/', { method: "get" }).then((response) => {
+  React.useEffect(()=>{
+    fetch('http://localhost:3000/clubs/', { method: "get" }).then((response) => {
       return response.json();
     }).then((data) => {
-      org_data = data;
+      // org_data = data;
     // sort the list of amenities here
-    var i, j, club_name, club_objID;
-    for(i=0;i<data.length;i++){
-      club_name = data[i].club_name;
-      club_objID = data[i]._id;
-      lookup[club_objID] = club_name;
-      for(j=0;j<data[i].activities.length;j++){
-        if(data[i].status=='1')
-          info.push({
-            id: club_objID+"-"+j,
-            name: data[i].activities[j].name,
-            description: data[i].activities[j].description,
-            category: data[i].activities[j].category,
-            booking_needed: data[i].activities[j].booking_needed,
-            club_name: club_objID
-          });
+      var i, j, club_name, club_objID;
+      for(i=0;i<data.length;i++){
+        club_name = data[i].club_name;
+        club_objID = data[i]._id;
+        lookup[club_objID] = club_name;
+        for(j=0;j<data[i].activities.length;j++){
+          if(data[i].status=='1' && data[i].activities[j].status==1)
+            info.push({
+              id: club_objID+"-"+j,
+              name: data[i].activities[j].name,
+              description: data[i].activities[j].description,
+              category: data[i].activities[j].category,
+              booking_needed: data[i].activities[j].booking_needed,
+              club_name: club_objID
+            });
+        }
       }
-    }
-  });
-  
-  columns = [
-    // {title: 'id', field:'id'},
-    { title: 'Activity', field: 'name'  },
-    { title: 'Description', field: 'description' },
-    { title: 'Category', field: 'category' },
-    { title: 'Booking required', field: 'booking_needed' , lookup: booking_lookup},
-    { title: 'Club Name', field: 'club_name' , lookup: lookup}
-  ];
+      columns = [
+        // {title: 'id', field:'id'},
+        { title: 'Activity', field: 'name'  },
+        { title: 'Description', field: 'description' },
+        { title: 'Category', field: 'category' },
+        { title: 'Booking required', field: 'booking_needed' , lookup: booking_lookup},
+        { title: 'Club Name', field: 'club_name' , lookup: lookup, editable: 'onAdd'}
+      ];
+    
+      info = Object.keys(info).map(i => info[i])
+      columns = Object.keys(columns).map(i => columns[i])
 
-  info = Object.keys(info).map(i => info[i])
-  columns = Object.keys(columns).map(i => columns[i])
+      setState({
+        columns: columns,
+        data: info
+      })
+    });
+
+  },[]);
   
-  const [state, setState] = React.useState({
-    columns: columns,
-    data: info
-  });
 
   return (
     <div>
@@ -143,7 +151,7 @@ function AllActivities() {
               resolve();
               setState((prevState) => {
                 const data = [...prevState.data];
-                console.log(newData);
+                // console.log(newData);
                 newData['id'] = new_row(newData);
                 data.push(newData);
                 return { ...prevState, data };
@@ -170,7 +178,7 @@ function AllActivities() {
               resolve();
               setState((prevState) => {
                 const data = [...prevState.data];
-                console.log(prevState.data[prevState.data.indexOf(oldData)]);
+                // console.log(prevState.data[prevState.data.indexOf(oldData)]);
                 delete_row(prevState.data[prevState.data.indexOf(oldData)].id);
                 data.splice(data.indexOf(oldData), 1);
                 return { ...prevState, data };
